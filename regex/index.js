0d63c44749dc8fd20eca62d8ba66e6face41d8df
Aijaz Abbasi <muhammadaijaz76@gmail.com>
2020-12-13 00:29:01 +0500

    regex

---
+++ regex/index.js
@@ -0,0 +1,23 @@
+
+let str1 = "hello this is test and after this test";
+
+let regex1 = /test/;
+
+
+console.log("log1",str1.match(regex1));
+
+
+let str2 = "test5";
+
+//  \d is for digit
+let regex2 = /\d/g
+
+console.log("log2",str2.match(regex2));
+
+let str3 = "atgd3783hgjh879";
+
+//  \b is for boundary
+let regx4 = /\d+/g; // ["3783","879"]
+
+console.log("log3", str3.match(regx4));
+// console.log("log4", str3.match(regx4));
